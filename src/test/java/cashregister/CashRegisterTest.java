bca2d759589a08eaac875a12f4c4a1c6ce5a88b7
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-26 17:40:46 +0800

    test3

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -2,21 +2,31 @@ package cashregister;
 
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
 
-
+	private static 	MockPrice mockPrice;
+	private static 	CashRegister cashRegister;
+	
+	
+@BeforeAll
+private static void setup() {
+	mockPrice = new MockPrice();
+	cashRegister = new CashRegister(mockPrice);
+}
+		
+	
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
     	Item item = new Item("test product",1);
     	Item[] items = {item};
     	Purchase purchase = new Purchase(items);
-    	MockPrice mockPrice = new MockPrice();
-    	CashRegister cashRegister = new CashRegister(mockPrice);
+
         //when
     	cashRegister.process(purchase);
     	String printString = mockPrice.getPrintString();
@@ -24,12 +34,15 @@ public class CashRegisterTest {
     	Assertions.assertEquals("test product\t1.0\n",printString);
     }
 
-//    @Test
-//    public void should_print_the_stub_purchase_when_call_process() {
-//        //given
-//        //when
-//        //then
-//    }
+    @Test
+    public void should_print_the_stub_purchase_when_call_process() {
+        //given
+    	StubPurchase stubPurchase = new StubPurchase();
+        //when
+    	cashRegister.process(stubPurchase);
+        //then
+    	Assertions.assertEquals("test product\t1.0\n",mockPrice.getPrintString());
+    }
 //
 //    @Test
 //    public void should_verify_with_process_call_with_mockito() {
