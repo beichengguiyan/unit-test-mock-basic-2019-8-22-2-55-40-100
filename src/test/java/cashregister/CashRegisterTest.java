2299ce69a0b3e4e1f128b017c85556654eebd5c0
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-26 17:02:32 +0800

    test

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,6 +1,7 @@
 package cashregister;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
@@ -11,22 +12,31 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+    	Item item = new Item("test product",1);
+    	Item[] items = {item};
+    	Purchase purchase = new Purchase(items);
+    	MockPrice mockPrice = new MockPrice();
+    	CashRegister cashRegister = new CashRegister(mockPrice);
         //when
+    	cashRegister.process(purchase);
+    	String printString = mockPrice.getPrintString();
+    	Assertions.assertEquals("test product\t1.0\n",printString);
+    	
         //then
     }
 
-    @Test
-    public void should_print_the_stub_purchase_when_call_process() {
-        //given
-        //when
-        //then
-    }
-
-    @Test
-    public void should_verify_with_process_call_with_mockito() {
-        //given
-        //when
-        //then
-    }
+//    @Test
+//    public void should_print_the_stub_purchase_when_call_process() {
+//        //given
+//        //when
+//        //then
+//    }
+//
+//    @Test
+//    public void should_verify_with_process_call_with_mockito() {
+//        //given
+//        //when
+//        //then
+//    }
 
 }
